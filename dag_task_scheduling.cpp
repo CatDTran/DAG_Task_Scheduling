77476b5d3e18337f0721ed0d0ad5a2b2611d0648
Carrot <trandinhcat@gmail.com>
2016-04-26 19:10:26 -0500

    Initial commit

---
+++ dag_task_scheduling.cpp
@@ -0,0 +1,26 @@
+/*Author: Cat Tran
+*Program: Task Scheduling using Directed Acylic Graph
+*Spring 2016 CSCE 2110-Program 3
+*University of North Texas*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <string>
+
+using namespace std;
+int numCrews = 0;
+int numNodes = 0;
+string line;
+int main()
+{
+	cin >> numCrews;
+	cout << numCrews << endl;
+	while(cin, line)
+	{
+		if(line[0] = 'c')
+		{
+			
+		}
+	}
+}
