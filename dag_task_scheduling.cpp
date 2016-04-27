9ae0e8c66983479b8964dd1ad0f9b3c416a2eb74
Carrot <trandinhcat@gmail.com>
2016-04-26 20:27:39 -0500

    Added: node.h

--- dag_task_scheduling.cpp
+++ dag_task_scheduling.cpp
@@ -1,7 +1,8 @@
 /*Author: Cat Tran
 *Program: Task Scheduling using Directed Acylic Graph
 *Spring 2016 CSCE 2110-Program 3
-*University of North Texas*/
+*University of North Texas
+*dag_task_scheduling.cpp*/
 
 #include <stdio.h>
 #include <iostream>
@@ -20,7 +21,7 @@ int main()
 	{
 		if(line[0] = 'c')
 		{
-			
+
 		}
 	}
 }
---
+++ node.h
@@ -0,0 +1,13 @@
+/*Author: Cat Tran
+*Program: Task Scheduling using Directed Acylic Graph
+*Spring 2016 CSCE 2110-Program 3
+*University of North Texas
+*node.h*/
+
+class Node
+{
+	int nodeName;
+	Node *predecessor;
+	Node *successor;
+	int distance
+}
